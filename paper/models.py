98965ab90ac99bedef23a8b5bcac8521c3eb24ac
Shahed595 <shahed.ahmed.cse@gmail.com>
2023-07-24 20:28:58 +0600

    models_done

--- paper/models.py
+++ paper/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class PaperStoreModel(models.Model):
+    COURSES=(
+         ('Math', 'Math'),
+        ('Eng', 'Eng'),
+        ('C++', 'C'),
+        ('Java', 'Java'),
+        ('Algorithm', 'Algorithm'),
+        ('Data Structure', 'Data Structure'),
+    )
+
+    id = models.IntegerField(primary_key=True)
+    student_Name = models.CharField(max_length=30)
+    teacher_name = models.CharField(max_length=30)
+    course_name = models.CharField(max_length=30, choices=COURSES)
+    department_name= models.CharField(max_length=30)
+    batch_name = models.CharField(max_length=30)
+    Section_name= models.CharField(max_length=30)
+    first_pub = models.TimeField(auto_now_add=True)
+    last_pub = models. TimeField(auto_now=True)
+    
