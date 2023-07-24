3ff08d250b152d4959bb88714368592b3f570969
Shahed595 <shahed.ahmed.cse@gmail.com>
2023-07-24 20:35:43 +0600

    form_done

---
+++ paper/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from paper.models import PaperStoreModel
+class PaperStoreForm(forms.ModelForm):
+    class Meta:
+        model = PaperStoreModel
+        fields = ['id', 'student_Name', 'teacher_name', 'course_name', 'department_name', 'batch_name',
+                    'Section_name', 'first_pub', 'last_pub']
+    
